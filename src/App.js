4701e2a024ec731001419a85ac6577568db2cf65
tarkaniskender <146021972+tarkaniskender@users.noreply.github.com>
2024-03-09 13:46:01 +0300

    feat: uploaded all changes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Repoyu Önce Github'ta oluşturdum.
+    </div>
+  );
+}
+
+export default App;
